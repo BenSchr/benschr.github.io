dc8cfae333a4f2f410630272d8a5326c47c57c8f
Benedikt Schröter <benedikt.schroeter@cologne-intelligence.de>
2020-03-08 19:26:26 +0100

    update

--- js/genalg/drawer.js
+++ js/genalg/drawer.js
@@ -5,40 +5,56 @@ class Drawer {
     }
 
 
-    drawPoints(arr,el) {
+    drawPoints(arr, el) {
         // console.log(arr)
         var x = []
         var y = []
-        
+
         for (var i = 0; i < arr.length; i++) {
             x.push(arr[i]["x"])
             y.push(arr[i]["y"])
         }
         var data = {
-            name:'Points',
+            name: 'Points',
             x: x,
             y: y,
             mode: 'markers',
-            marker:{color:'green'}
+            marker: { color: 'white',size:10 }
         }
-        var layout = {hovermode:'closest',
-        margin: {
-            l: 40,
-            r: 20,
-            b: 20,
-            t: 20,
-            pad: 4
-          },     
-          showlegend: false,
-        automargin: false};
+        var layout = {
+            hovermode: 'closest',
+            margin: {
+                l: 40,
+                r: 20,
+                b: 20,
+                t: 20,
+                pad: 4
+            }, 
+            autosize:true,
+            paper_bgcolor: 'rgba(0,0,0,0)',
+            plot_bgcolor: 'rgba(0,0,0,0)',
+            font:{color:"white"},
+            showlegend: false,
+            automargin: false,
 
-        var config = {responsive: true,'displayModeBar': false}
+            xaxis:{
+                fixedrange:true,
+                gridcolor: 'rgba(255,255,255,0.5)',
+            },
+            yaxis:{
+                fixedrange:true,
+                gridcolor: 'rgba(255,255,255,0.5)',
+            }
+
+        };
+
+        var config = { responsive: true, 'displayModeBar': false }
 
 
-        Plotly.newPlot(el,[data],layout,config);
+        Plotly.newPlot(el, [data], layout, config);
     }
 
-    drawLines(arr,el) {
+    drawLines(arr, el) {
         var canvas = el
         var x = []
         var y = []
@@ -51,66 +67,99 @@ class Drawer {
         y.push(arr[0]["y"])
 
         var data1 = {
-            name:'Path',
+            
             x: x,
             y: y,
             mode: 'lines'
         }
         var data2 = {
-            name:'Points',
+            
             x: x,
             y: y,
             mode: 'markers',
-            marker:{color:'green'}
+            marker: { color: 'white',size:10 }
         }
-        var layout = {hovermode:'closest',
-        margin: {
-            l: 40,
-            r: 20,
-            b: 20,
-            t: 20,
-            pad: 4
-          },     
-          showlegend: false,  
-        automargin: false};
+        var layout = {
+            hovermode: 'closest',
+            margin: {
+                l: 40,
+                r: 20,
+                b: 20,
+                t: 20,
+                pad: 4
+            },
+            autosize:true,
+            paper_bgcolor: 'rgba(0,0,0,0)',
+            plot_bgcolor: 'rgba(0,0,0,0)',
+            font:{color:"white"},
+            showlegend: false,
+            automargin: false,
+            xaxis:{
+                fixedrange:true,
+                gridcolor: 'rgba(255,255,255,0.5)',
+            },
+            yaxis:{
+                fixedrange:true,
+                gridcolor: 'rgba(255,255,255,0.5)',
+            }
+        };
 
-        var config = {responsive: true,
-            'displayModeBar': false}
+        var config = {
+            responsive: true,
+            'displayModeBar': false
+        }
 
         var data = [data1, data2]
-        Plotly.newPlot(canvas, data,layout,config);
+        Plotly.newPlot(canvas, data, layout, config);
 
 
     }
 
-    drawPerformance(obj,el){
+    drawPerformance(obj, el) {
         var canvas = el
         var x = obj["x"]
         var y = obj["y"]
 
 
         var data = {
-            name:'Path',
+            name: 'Path',
             x: x,
             y: y,
             mode: 'lines'
         }
-        var layout = {hovermode:'closest',
-        margin: {
-            l: 40,
-            r: 20,
-            b: 20,
-            t: 20,
-            pad: 4
-          },     
-          showlegend: false,  
-        automargin: false};
+        var layout = {
+            hovermode: 'closest',
+            margin: {
+                l: 40,
+                r: 20,
+                b: 20,
+                t: 20,
+                pad: 4
+            },
+            autosize:true,
+            paper_bgcolor: 'rgba(0,0,0,0)',
+            plot_bgcolor: 'rgba(0,0,0,0)',
+            font:{color:"white"},
+            showlegend: false,
+            automargin: true,
+            xaxis:{
+                fixedrange:true,
+                gridcolor: 'rgba(255,255,255,0.5)',
+            },
+            yaxis:{
+                fixedrange:true,
+                gridcolor: 'rgba(255,255,255,0.5)',
+            }
 
-        var config = {responsive: true,
-            'displayModeBar': false}
+        };
 
-        
-        Plotly.newPlot(canvas, [data],layout,config);
+        var config = {
+            responsive: true,
+            'displayModeBar': false
+        }
+
+
+        Plotly.newPlot(canvas, [data], layout, config);
     }
 
 
