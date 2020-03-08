32d5464fefe7470bbe6f35ace50d46a1ada0d52f
Benedikt Schröter <benedikt.schroeter@cologne-intelligence.de>
2020-03-08 16:23:47 +0100

    update

--- js/genalg/drawer.js
+++ js/genalg/drawer.js
@@ -1,35 +1,116 @@
-class Drawer{
-    constructor(canvas){
-        this.canvas=canvas
-        this.ctx=canvas.getContext("2d");
-        this.ctx.lineWidth=8
+class Drawer {
+    constructor() {
+
+
     }
 
 
-    drawPoints(arr){
-        this.ctx.fillStyle = "white";
-        this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height)
-        this.ctx.fillStyle = "black";
-        for(var i=0; i<arr.length; i++){
-            var x=arr[i]["x"]
-            var y=arr[i]["y"]
-            this.ctx.fillRect(x, y, 10, 10)
+    drawPoints(arr,el) {
+        // console.log(arr)
+        var x = []
+        var y = []
+        
+        for (var i = 0; i < arr.length; i++) {
+            x.push(arr[i]["x"])
+            y.push(arr[i]["y"])
         }
+        var data = {
+            name:'Points',
+            x: x,
+            y: y,
+            mode: 'markers',
+            marker:{color:'green'}
+        }
+        var layout = {hovermode:'closest',
+        margin: {
+            l: 40,
+            r: 20,
+            b: 20,
+            t: 20,
+            pad: 4
+          },     
+          showlegend: false,
+        automargin: false};
+
+        var config = {responsive: true,'displayModeBar': false}
+
+
+        Plotly.newPlot(el,[data],layout,config);
     }
 
-    drawLines(arr){
-        
-        this.ctx.beginPath()
-            var offset= 10 /2
-            for (var i = 0; i < arr.length; i++) {
-                var nextobj = (i + 1) % arr.length
-                this.ctx.moveTo(arr[i]["x"] + offset, arr[i]["y"] +offset)
-                this.ctx.lineTo(arr[nextobj]["x"] + offset, arr[nextobj]["y"] +offset)
-            }
-            this.ctx.closePath()
-        
-            this.ctx.stroke()
+    drawLines(arr,el) {
+        var canvas = el
+        var x = []
+        var y = []
 
+        for (var i = 0; i < arr.length; i++) {
+            x.push(arr[i]["x"])
+            y.push(arr[i]["y"])
+        }
+        x.push(arr[0]["x"])
+        y.push(arr[0]["y"])
+
+        var data1 = {
+            name:'Path',
+            x: x,
+            y: y,
+            mode: 'lines'
+        }
+        var data2 = {
+            name:'Points',
+            x: x,
+            y: y,
+            mode: 'markers',
+            marker:{color:'green'}
+        }
+        var layout = {hovermode:'closest',
+        margin: {
+            l: 40,
+            r: 20,
+            b: 20,
+            t: 20,
+            pad: 4
+          },     
+          showlegend: false,  
+        automargin: false};
+
+        var config = {responsive: true,
+            'displayModeBar': false}
+
+        var data = [data1, data2]
+        Plotly.newPlot(canvas, data,layout,config);
+
+
+    }
+
+    drawPerformance(obj,el){
+        var canvas = el
+        var x = obj["x"]
+        var y = obj["y"]
+
+
+        var data = {
+            name:'Path',
+            x: x,
+            y: y,
+            mode: 'lines'
+        }
+        var layout = {hovermode:'closest',
+        margin: {
+            l: 40,
+            r: 20,
+            b: 20,
+            t: 20,
+            pad: 4
+          },     
+          showlegend: false,  
+        automargin: false};
+
+        var config = {responsive: true,
+            'displayModeBar': false}
+
+        
+        Plotly.newPlot(canvas, [data],layout,config);
     }
 
 
